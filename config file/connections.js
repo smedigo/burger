12efe68a7dbb62d1b2e5d4d9a27d62af77ed23f8
smedigo <smedigo@gmail.com>
2021-01-21 15:36:23 -0800

    setting up connections and orm

---
+++ config file/connections.js
@@ -0,0 +1,28 @@
+const util = require('util');
+const mysql = require('mysql');
+
+let connection;
+// HEROKU connection
+if ( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'tanya555',
+    database: 'burgersDB'
+});
+};
+
+connection.connect(err => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+      }
+    console.log ("connect as id " + connection.threadId);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
